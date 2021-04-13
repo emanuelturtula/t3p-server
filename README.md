7203ab313770554f2a2f3e62a558798b7067486d
Emanuel Turtula <emanuelturtula@gmail.com>
2021-04-13 13:43:12 -0300

    Start documentation. Fix clear config and makefile

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# -t3p-server
-T3P server Repository for Laboratorio de Redes de Computadoras - FIUBA
+# TicTacToe Server
+
+In this repository, you will find the T3P Server. This was a project requested in the course Laboratorio de Redes de Computadoras, from FIUBA.
+
+# How to run
+
+This server needs a linux kernel based OS in order to be compiled and executed. Before compiling, take 
+
+In the root folder, you will find a bash script: 'make_server.sh'
--- headers/config.h
+++ headers/config.h
@@ -1,6 +1,3 @@
-//#define DEBUG_MESSAGES
-//#define DEBUG_TEST
-
 #define SERVER_IP "127.0.0.1"
 
 #define UDP_PORT_NUMBER 2001
@@ -14,22 +11,3 @@
 #define HEARTBEAT_SECONDS_TIMEOUT 10
 #define MARKSLOT_SECONDS_TIMEOUT 30
 #define INVITATION_SECONDS_TIMEOUT 10
-
-// Colours
-#define RESET   "\033[0m"
-#define BLACK   "\033[30m"      /* Black */
-#define RED     "\033[31m"      /* Red */
-#define GREEN   "\033[32m"      /* Green */
-#define YELLOW  "\033[33m"      /* Yellow */
-#define BLUE    "\033[34m"      /* Blue */
-#define MAGENTA "\033[35m"      /* Magenta */
-#define CYAN    "\033[36m"      /* Cyan */
-#define WHITE   "\033[37m"      /* White */
-#define BOLDBLACK   "\033[1m\033[30m"      /* Bold Black */
-#define BOLDRED     "\033[1m\033[31m"      /* Bold Red */
-#define BOLDGREEN   "\033[1m\033[32m"      /* Bold Green */
-#define BOLDYELLOW  "\033[1m\033[33m"      /* Bold Yellow */
-#define BOLDBLUE    "\033[1m\033[34m"      /* Bold Blue */
-#define BOLDMAGENTA "\033[1m\033[35m"      /* Bold Magenta */
-#define BOLDCYAN    "\033[1m\033[36m"      /* Bold Cyan */
-#define BOLDWHITE   "\033[1m\033[37m"      /* Bold White */
